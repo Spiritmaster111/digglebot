02cd15b75e766f7ef2706bdf2145926091874833
Nick Algra <nickalgra@hotmail.com>
2018-12-16 01:54:58 +0100

    Update bot.js
    oh boy here we go

--- bot.js
+++ bot.js
@@ -4,12 +4,30 @@ const client = new Discord.Client();
 client.on("ready", () => {
 	console.log("I am ready!");
 	
-	mainChannel = client.channels.get("398995797753987085");
-	dbChannel = client.channels.get("399633585193353236");
+	dbChannel = client.channels.get("523655389611556874");
+	channelList = [];
+	channelList[0] = client.channels.get("523653674350804992");
+	channelList[1] = client.channels.get("523653688343003136");
+	channelList[2] = client.channels.get("523653701727158292");
+	channelList[3] = client.channels.get("523653712577560581");
+	channelList[4] = client.channels.get("523653724183199762");
+	channelList[5] = client.channels.get("523653733922373642");
+	channelList[6] = client.channels.get("523653744412590083");
+	channelList[7] = client.channels.get("523653757754408973");
+	channelList[8] = client.channels.get("523653770505224202");
+	channelList[9] = client.channels.get("523653781875982347");
+	channelList[10] = client.channels.get("523653794572271657");
+	channelList[11] = client.channels.get("523653805829652490");
+	channelList[12] = client.channels.get("523653819041841164");
+	channelList[13] = client.channels.get("523653835974115349");
+	channelList[14] = client.channels.get("523653852927361061");
 	
-	mainChannel.fetchPinnedMessages().then(messages => {
-		dataMsg = messages.last();
-		data = JSON.parse(dataMsg);
+	dbChannel.fetchMessages().then(messages => {
+		dataMsgList = messages.array();
+		dataList = [];
+		for (var i = 0; i < dataMsgList.length; i++) {
+			dataList[i] = JSON.parse(dataMsgList[i]);
+		}
 		
 		/*var listMsgs = messages.array();
 		list = new Array(data.num);
@@ -19,9 +37,12 @@ client.on("ready", () => {
 		}*/
 	});
 	
-	dbChannel.fetchMessages().then(messages => {
-		list = messages.array();
-	});
+	list = [];
+	for (var i = 0; i < channelList.length; i++) {
+		channelList[i].fetchMessages().then(messages => {
+			listList[i] = messages.array();
+		});
+	}
 	
 	client.user.setPresence({game: {name: "without consumables", type: 0}});
 	client.user.setStatus("online");
@@ -40,49 +61,56 @@ client.on("message", (message) => {
 		} else if (command === 'foo') {
 			message.channel.send("Bar!\nWho goes tharr?");
 		} else if (command === 'list') {
-			if (!data[message.author.id]) data[message.author.id] = {"Used":0,"Score":0};
-			if (data[message.author.id].used >= data.len) {
-				message.channel.send("Sorry, no addresses left!");
-			} else { 
-				var stringList = "Your list of " + (list.length - data[message.author.id].used) + " unused addresses:";
-				for (var i = list.length-1-data[message.author.id].used; i >= 0; i--) {
-					stringList = stringList + "\n" + list[i];
+			if (args.length < 1) {
+				message.channel.send("Needs more level!");
+			} else {
+				if (!dataList[args[0]][message.author.id]) dataList[args[0]][message.author.id] = {"Used":0,"Score":0};
+				
+				/*if (dataList[args[0]][message.author.id].used >= data.len) {
+					message.channel.send("Sorry, no addresses left!");
+				} else { */
+				
+				var stringList = "Your list of " + (listList[args[0]].length - dataList[args[0]][message.author.id].used) + " unused addresses:";
+				for (var i = listList[args[0]].length-1-data[message.author.id].used; i >= 0; i--) {
+					stringList = stringList + "\n" + listList[args[0]][i];
 				}
 				message.channel.send(stringList);
+				
+				//}
 			}
-		} else if (command === 'score') {
+		/*} else if (command === 'score') {
 			var subject = message.mentions.users.first();
 			if (!subject) subject = message.author;
 			if (!data[subject.id]) data[subject.id] = {"Used":0,"Score":0};
 			const score = data[subject.id].score;
-			message.channel.send(subject.toString() + " has added a grand total of " + score + " addresses to the list!");
+			message.channel.send(subject.toString() + " has added a grand total of " + score + " addresses to the list!");*/
 		} else if (command === 'add') {
 			for (var i = 0; i < args.length; i++) {
-				list.unshift(args[i]);
+				listList[args[0]].unshift(args[i]);
 				dbChannel.send(args[i]);
-				list.pop();
-				for (var i in data) {
-					if (data[i].used > 0) data[i].used--;
+				listList[args[0]].pop();
+				for (var i in dataList[args[0]]) {
+					if (dataList[args[0]][i].used > 0) dataList[args[0]][i].used--;
 				}
-				data[message.author.id].score++;
+				//dataList[args[0]][message.author.id].score++;
 			}
-			dataMsg.edit(JSON.stringify(data));
+			dataMsgList[args[0]].edit(JSON.stringify(dataList[args[0]]));
 			if (args.length > 1) {
 				message.channel.send("Gotcha, added em to the list!");
 			} else {
 				message.channel.send("Gotcha, added it to the list!");
 			}
 		} else if (command === 'use') {
-			if (!data[message.author.id]) data[message.author.id] = {"Used":0,"Score":0};
-			if (data[message.author.id].used >= list.length) {
+			if (!dataList[args[0]][message.author.id]) dataList[args[0]][message.author.id] = {"Used":0,"Score":0};
+			if (dataList[args[0]][message.author.id].used >= listList[args[0]].length) {
 				message.channel.send("Sorry, no addresses left!");
 			} else {
-				message.channel.send("\"" + list[list.length-data[message.author.id].used-1] + "\" is all yours!");
-				data[message.author.id].used++;
-				dataMsg.edit(JSON.stringify(data));
+				message.channel.send("\"" + listList[args[0]][listList[args[0]].length-dataList[args[0]][message.author.id].used-1] + "\" is all yours!");
+				dataList[args[0]][message.author.id].used++;
+				dataMsgList[args[0]].edit(JSON.stringify(dataList[args[0]]));
 			}
 		} else if (command === 'dump') {
-			dataMsg.edit(JSON.stringify(data));
+			dataMsgList[args[0]].edit(JSON.stringify(dataList[args[0]]));
 		/*} else if (command === 'echo') {
 			var msgID = client.channels.get("398995797753987085").fetchPinnedMessages().then(messages => {
 				const fetchedMsg = messages.first();
@@ -102,12 +130,12 @@ client.on("message", (message) => {
 			for (var i = 0; i < list.length; i++) {
 				message.channel.send(list[i].content);
 			}*/
-		} else if (command === 'delistify') {
+		/*} else if (command === 'delistify') {
 			const delistifyMe = args[0];
 			const delistified = delistifyMe.split("\n");
 			for (var i = 0; i < delistified.length; i++) {
 				dbChannel.send(delistified[i]);
-			}
+			}*/
 		}
 	}
 });
