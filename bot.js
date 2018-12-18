51649d4fe74fd854ed7177268a21b7c318747a2e
Nick Algra <nickalgra@hotmail.com>
2018-12-18 11:07:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,12 +80,11 @@ client.on("message", (message) => {
 				message.channel.send("Sorry, no unused addresses left!");
 			} else {
 				var tarChannel = client.channels.get(channelIDs[lvl]);
-				var list;
 				tarChannel.fetchMessages().then(messages => {
-					list = messages.array();
+					var list = messages.array();
+					var address = list[Math.max(50, data.len) - data[message.author.id].used - 1];
+					message.channel.send("\"" + address + "\" is all yours!");
 				});
-				var address = list[Math.max(50, data.len) - data[message.author.id].used - 1];
-				message.channel.send("\"" + address + "\" is all yours!");
 				data[message.author.id].used++;
 			}
 			dataMsg.edit(JSON.stringify(data));
